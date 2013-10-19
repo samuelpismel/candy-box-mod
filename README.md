4a0b7f7cb12c4062cd0f3a0d1691c962a4e87e4f
Samuel Gavassi Pismel <samuel.pismel@gmail.com>
2013-10-19 00:30:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Candy Box Mod
+
+This mod was made for the [Candy Box Game](http://candies.aniwey.net/) to improve the user experience and make the game more fun.
+
+This mod doesn't change the game system, so its not a kind of cheat.
+
+## Features
+
+- Top bar
+- Improved save system
+- Load system
+
+## Installation
+
+Drag the installation link to the bookmark bar of your navigator, open the [Candy Box page](http://candies.aniwey.net/) and click on the **Candy Box Mod** of your bookmark.
+
+So the mod will be loaded in the game.
+
+Installation link (to drag): [Candy Box Mod](javascript:%28function%28%29%7B%20var%20d%20%3D%20document%2Cs%20%3D%20d.createElement%28%27script%27%29%2Ct%20%3D%20d.body%20%7C%7C%20d.getElementsByTagName%28%27head%27%29%5B0%5D%20%7C%7C%20d.documentElement%3Bs.src%20%3D%20%27http%3A//localhost/candy-box-extension/script.js%27%3Bt.appendChild%28s%29%3B%7D%29%28%29%3B)
+
+## Top Bar
+
+A top bar will be created when the mod loads. This bar will contain information about your current candy stock and buttons to save and load your game.
+
+## Improved save system
+
+With the **Save Button** in the **Top Bar**, now you can save your game any time you want, without going to the **Candy Box Tab**
+
+When you click in the save button, it will be green highlighted if the game saves sucessfully, and you will not see the annoying window telling you about *the code*. ;)
+
+Your *save code* will be saved in a cookie in your browser and you will be able to load it after.
+
+## Load system
+
+With *your code* saved in a cookie, you can simply click in the **Load Button** and play your game.
+
+## TODO
+
+- Hotkeys for itens
+- Settings
+- Save on close
+- Auto-save
+- Profile system (slots for multiple saves)
+
+- - -
+Created by: Samuel Gavassi Pismel
---
+++ script.js
@@ -0,0 +1,267 @@
+console.log('Candybox Mod Initializing!')
+
+var Mod = {
+  system: {
+    load: {
+      script: function(src) {
+        var script, parent;
+        script = document.createElement('script')
+        script.type = 'text/javascript'
+        script.src = src;
+        parent = document.getElementsByTagName('head')[0];
+        parent.appendChild(script);
+        return script;
+      },
+      stylesheet: function(src) {
+        var link, parent;
+        link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.href = src;
+        parent = document.getElementsByTagName('head')[0];
+        parent.appendChild(link);
+        return link;
+      }
+    },
+    cookies: {
+      create: function(name,value,days) {
+        if (days) {
+          var date = new Date();
+          date.setTime(date.getTime()+(days*24*60*60*1000));
+          var expires = "; expires="+date.toGMTString();
+        }
+        else var expires = "";
+        document.cookie = name+"="+value+expires+"; path=/";
+      },
+      read: function(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+          var c = ca[i];
+          while (c.charAt(0)==' ') c = c.substring(1,c.length);
+          if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+      },
+      erase: function(name) {
+        createCookie(name,"",-1);
+      }
+    }
+  },
+  game: {
+    data: function() {
+      return {
+        code: ((code === undefined || code == null || code.length == "") ? 0 : code),
+        swordName: sword.name,
+        swordSpecialSword: Number(sword.specialSword),
+        swordSpecialPower: sword.specialPower,
+        candiesNbrOwned: candies.nbrOwned,
+        candiesNbrThrown: candies.nbrThrown,
+        candiesNbrEaten: candies.nbrEaten,
+        candiesNbrTotal: candies.nbrTotal,
+        candiesCandiesPerSecond: candies.candiesPerSecond,
+        candiesConverterActivated: Number(candiesConverter.activated),
+        cauldronBookPage: cauldron.bookPage,
+        cauldronCandies: cauldron.candiesInTheCauldron,
+        cauldronLollipops: cauldron.lollipopsInTheCauldron,
+        chocolateBarsNbrOwned: chocolateBars.nbrOwned,
+        farmLollipopsPlanted: farm.lollipopsPlanted,
+        farmCurrentFlagIndex: farm.currentFlagIndex,
+        farmPlantingButtonsStep: farm.plantingButtonsStep,
+        forgeStep: forge.step,
+        shopLollipopsButtonsShown: Number(shop.buy10LollipopsButtonShown),
+        shopShown: Number(shop.shown),
+        shopTicklingStep: shop.ticklingStep,
+        shopClickingOnLollipopStep: shop.clickingOnLollipopStep,
+        hutStep: hut.step,
+        hutSpeech: hut.speech,
+        inventoryMagicianHatLetter: inventory.magicianHatLetter,
+        lollipopsNbrOwned: lollipops.nbrOwned,
+        lollipopsNbrInStock: lollipops.nbrInStock,
+        lollipopsNbrBought: lollipops.nbrBought,
+        mainNbrOfSecondsSinceLastMinInterval: main.nbrOfSecondsSinceLastMinInterval,
+        mainNbrOfSecondsSinceLastHourInterval: main.nbrOfSecondsSinceLastHourInterval,
+        mainNbrOfSecondsSinceLastDayInterval: main.nbrOfSecondsSinceLastDayInterval,
+        mountGoblinBasicChestProbability: mountGoblin.basicChestProbability,
+        peacefulForestBasicChestProbability: peacefulForest.basicChestProbability,
+        peacefulForestPoniesEncountered: peacefulForest.poniesEncountered,
+        objectsHaveObjectKey: Number(objects.list.key.have),
+        objectsHaveObjectHutMap: Number(objects.list.hutMap.have),
+        objectsHaveObjectWellMap: Number(objects.list.wellMap.have),
+        objectsHaveObjectSwampMap: Number(objects.list.swampMap.have),
+        objectsHaveObjectBoots: Number(objects.list.boots.have),
+        objectsHaveObjectMagicianHat: Number(objects.list.magicianHat.have),
+        objectsHaveObjectPinkRing: Number(objects.list.pinkRing.have),
+        objectsHaveObjectForgeMap: Number(objects.list.forgeMap.have),
+        objectsHaveObjectCandiesConverter: Number(objects.list.candiesConverter.have),
+        objectsHaveObjectPlateArmour: Number(objects.list.plateArmour.have),
+        objectsHaveObjectCauldron: Number(objects.list.cauldron.have),
+        objectsHaveObjectMagicalHorn: Number(objects.list.magicalHorn.have),
+        objectsHaveObjectHornOfPlenty: Number(objects.list.hornOfPlenty.have),
+        objectsHaveObjectOldAmulet: Number(objects.list.oldAmulet.have),
+        potionsShownHealth: Number(potions.list.health.shown),
+        potionsShownEscape: Number(potions.list.escape.shown),
+        potionsShownBerserk: Number(potions.list.berserk.shown),
+        potionsShownFireScroll: Number(potions.list.fireScroll.shown),
+        potionsShownAcidRainScroll: Number(potions.list.acidRainScroll.shown),
+        potionsShownTeleportScroll: Number(potions.list.teleportScroll.shown),
+        potionsShownEarthquakeScroll: Number(potions.list.earthquakeScroll.shown),
+        potionsShownImpInvocationScroll: Number(potions.list.impInvocationScroll.shown),
+        potionsShownMajorHealth: Number(potions.list.majorHealth.shown),
+        potionsShownInvulnerability: Number(potions.list.invulnerability.shown),
+        potionsShownTurtle: Number(potions.list.turtle.shown),
+        potionsShownJelly: Number(potions.list.jelly.shown),
+        potionsShownSeed: Number(potions.list.seed.shown),
+        potionsShownCloning: Number(potions.list.cloning.shown),
+        potionsShownSuperman: Number(potions.list.superman.shown),
+        potionsShownGmooh: Number(potions.list.gmooh.shown),
+        potionsNbrOwnedHealth: potions.list.health.nbrOwned,
+        potionsNbrOwnedEscape: potions.list.escape.nbrOwned,
+        potionsNbrOwnedBerserk: potions.list.berserk.nbrOwned,
+        potionsNbrOwnedFireScroll: potions.list.fireScroll.nbrOwned,
+        potionsNbrOwnedAcidRainScroll: potions.list.acidRainScroll.nbrOwned,
+        potionsNbrOwnedTeleportScroll: potions.list.teleportScroll.nbrOwned,
+        potionsNbrOwnedEarthquakeScroll: potions.list.earthquakeScroll.nbrOwned,
+        potionsNbrOwnedImpInvocationScroll: potions.list.impInvocationScroll.nbrOwned,
+        potionsNbrOwnedMajorHealth: potions.list.majorHealth.nbrOwned,
+        potionsNbrOwnedInvulnerability: potions.list.invulnerability.nbrOwned,
+        potionsNbrOwnedTurtle: potions.list.turtle.nbrOwned,
+        potionsNbrOwnedJelly: potions.list.jelly.nbrOwned,
+        potionsNbrOwnedSeed: potions.list.seed.nbrOwned,
+        potionsNbrOwnedCloning: potions.list.cloning.nbrOwned,
+        potionsNbrOwnedSuperman: potions.list.superman.nbrOwned,
+        potionsNbrOwnedGmooh: potions.list.gmooh.nbrOwned,
+        questMaxLandOrder: quest.maxLandOrder,
+        questTiredTime: quest.tiredTime,
+        spellsFasterCandiesFibo1: spells.fasterCandiesFiboPrev,
+        spellsFasterCandiesFibo2: spells.fasterCandiesFiboCurr,
+        swampStep: swamp.step,
+        tabsAnimation: tabs.animation,
+        wishingWellSpeech: wishingWell.speech,
+        wishingWellStep: wishingWell.step,
+        yourselfCanSurpass: Number(yourself.canSurpass),
+        developperComputerWon: Number(developperComputer.won)
+      }
+    },
+    save: function() {
+      var button = $('#mod_save');
+      $.post("scripts/save.php", Mod.game.data(), function(msg) {
+        if (msg != "Erreur" && msg.substring(0,5) != "<br /") {
+          code = msg.substring(0,5);
+          Mod.system.cookies.create('session', code);
+          button.css({backgroundColor: '#00bb00'}).animate({backgroundColor: '#ddd'}, 1000)
+        } else {
+          alert("There was a problem while saving. Please try again later :/");    
+          button.css({backgroundColor: '#ff0000'}).animate({backgroundColor: '#ddd'}, 1000)
+        }
+      });      
+    },
+    load: function() {
+      var code = Mod.system.cookies.read('session');
+      if (code) {
+        window.location.href = "/index.php?pass=" + code
+      } 
+    }
+  },
+  top_bar: {
+    interval: {
+      id: null,
+      loop: function() {
+        var format = '0,0'
+        Mod.top_bar.mod_candies.text(numeral(candies.nbrOwned).format(format));
+        Mod.top_bar.mod_lollipops.text(numeral(lollipops.nbrOwned).format(format));
+      },
+      setUp: function() {
+        Mod.top_bar.interval.id = window.setInterval(Mod.top_bar.interval.loop, 100);
+      }
+    },
+    setUp: function () {
+      var top_html = '';
+      top_html += '<div class="mod_top_bar">';
+      top_html += '<button id="mod_save">Save</button>';
+      top_html += '<button id="mod_load">Load</button>';
+      top_html += '<span>Candies: <span id="mod_candies_qty"></span></span>';
+      top_html += '<span> | </span>';
+      top_html += '<span>Lollipops: <span id="mod_lollipops_qty"></span></span>';
+      top_html += '</div>';
+
+      $('body').prepend(top_html);
+
+      Mod.top_bar.mod_candies = $('#mod_candies_qty');
+      Mod.top_bar.mod_lollipops = $('#mod_lollipops_qty');
+
+      $('#mod_save').on('click', Mod.game.save)
+      $('#mod_load').on('click', Mod.game.load)
+
+      Mod.top_bar.interval.setUp();
+
+      console.log('Top bar initialized!')
+    }
+  },
+  hotkeys: {
+    setUp: function(){
+      var buttons = $('#quest_potions button');
+      $(buttons[0]).prop('id', 'seed_potion');
+
+      $(buttons[1]).prop('id', 'helth_potion');
+      $(buttons[2]).prop('id', 'escape_potion');
+      $(buttons[3]).prop('id', 'berserk_potion');
+
+      $(buttons[4]).prop('id', 'major_health_potion');
+      $(buttons[5]).prop('id', 'invulnerability_potion');
+      $(buttons[6]).prop('id', 'superman_potion');
+      $(buttons[7]).prop('id', 'cloning_potion');
+      $(buttons[8]).prop('id', 'gmooh_potion');
+
+      $(buttons[9]).prop('id', 'fire_scroll');
+      $(buttons[10]).prop('id', 'acid_scroll');
+      $(buttons[11]).prop('id', 'teleport_scroll');
+      $(buttons[12]).prop('id', 'imp_scroll');
+      $(buttons[13]).prop('id', 'earthquake_scroll');
+
+
+      $(window).off('keypress', Mod.hotkeys.onKeyPress)
+      $(window).on('keypress', Mod.hotkeys.onKeyPress)
+      console.log('Hotkeys binded!')
+    },
+    keys: {
+      48: 'seed_potion', // 0
+
+      49: 'helth_potion', // 1
+      50: 'escape_potion', // 2
+      51: 'berserk_potion', // 3
+
+      52: 'major_health_potion', //4
+      53: 'invulnerability_potion', // 5
+      54: 'superman_potion', // 6
+      55: 'cloning_potion', // 7
+      56: 'gmooh_potion', // 8
+
+      113: 'fire_scroll', // q
+      119: 'acid_scroll',
+      101: 'teleport_scroll',
+      97: 'imp_scroll',
+      115: 'earthquake_scroll',
+    },
+    onKeyPress: function (event) {
+      var keys = Mod.hotkeys.keys;
+      console.log(keys[event.charCode], $('.'+keys[event.charCode]))
+      if (keys[event.charCode] != undefined)
+        $('#'+keys[event.charCode]).click()
+    }
+  },  
+  loadFiles: function() {
+    Mod.system.load.stylesheet('http://localhost/candy-box-extension/style.css');
+    Mod.system.load.script('http://raw.github.com/jquery/jquery-color/master/jquery.color.js');
+    Mod.system.load.script('http://cdnjs.cloudflare.com/ajax/libs/numeral.js/1.4.5/numeral.min.js');
+  },  
+  setUp: function() {
+    Mod.loadFiles();
+    Mod.top_bar.setUp();
+    Mod.hotkeys.setUp();
+  }
+};
+
+Mod.setUp();
+
+console.log('Candybox Mod loaded!')
